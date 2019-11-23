55cb0d0ccaa5d137eaa6afb2a8f27e780ceadc90
Tony Xu <Design.TXU@gmail.com>
2019-11-23 21:14:10 +1100

    Customer Object Saves Into Database

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator')
 
+const Customer = require('../models/Customer');
+
 // GET REQUEST, CREATE, ADD, DELETE, UPDATE.
 
 router.post('/', [
@@ -11,13 +13,37 @@ router.post('/', [
   check('email', 'Valid Email Required')
     .isEmail()
 ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('Customer Route');
+
+    const { name, dob, email, phone } = req.body;
+
+    try {
+      let customer = await Customer.findOne({ email });
+
+      if (customer) {
+        return res.status(400).json({ errors: [{ msg: 'Email already exists' }] });
+      }
+
+      customer = new Customer({
+        name,
+        dob,
+        email,
+        phone
+      });
+
+      await customer.save();
+
+      console.log(req.body);
+      res.send('Customer Added');
+
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
   })
 
 module.exports = router;
