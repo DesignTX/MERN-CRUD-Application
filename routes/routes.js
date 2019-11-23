7fcbbafaf0303cf388136225b854257312019d76
Tony Xu <Design.TXU@gmail.com>
2019-11-23 23:49:16 +1100

    CRUD Added

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,12 @@ const { check, validationResult } = require('express-validator')
 
 const Customer = require('../models/Customer');
 
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find({})
+  res.status(200).send(customers);
+});
+
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -53,6 +59,51 @@ router.post('/', [
       res.status(500).send('Server Error');
     }
   }
-)
+);
+
+
+router.put("/:id", [
+  check('name', 'Name Required')
+    .not()
+    .isEmpty(),
+  check('email', 'Valid Email Required')
+    .isEmail()
+],
+  async (req, res) => {
+    const { id } = req.params;
+    const foundCustomer = await Customer.findOne({ _id: id })
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const updatedCustomerDetails = {
+      name: req.body.name,
+      dob: req.body.dob,
+      email: req.body.email,
+      phone: req.body.phone
+    };
+    try {
+      const updatedCustomer = await Customer.findByIdAndUpdate
+        // { new: true} sends back updated customer
+        (req.params.id, updatedCustomerDetails, { new: true });
+      res.status(200).send(updatedCustomer)
+    } catch (err) {
+      return res.status(400).send(`Customer updating error ${err}`)
+    }
+  }
+);
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedCustomer = await Customer.findByIdAndDelete(id)
+    return res.status(200).send(`Customer deleted ${deletedCustomer.name}`)
+  } catch (err) {
+    return res.status(400).send(`Error deleting customer ${err}`)
+  }
+});
+
+
 
 module.exports = router;
