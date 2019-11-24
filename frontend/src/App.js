92ab9636636d81307f8c1e252811046fd3b3fcbf
Tony Xu <Design.TXU@gmail.com>
2019-11-24 17:12:34 +1100

    Setup Redux Store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,27 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Registration from './components/Registration';
 import Edit from './components/Edit';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/registration" component={Registration} />
-          <Route exact path="/edit" component={Edit} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/registration" component={Registration} />
+            <Route exact path="/edit" component={Edit} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 );
 
 export default App;
